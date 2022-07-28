29c78a69cbbed1937329801ff909ebb838eadae5
letnagao <leticia-nagao@hotmail.com>
2022-07-28 00:30:20 -0300

    starting about section

--- js/app.js
+++ js/app.js
@@ -7,3 +7,11 @@ function stickyNavbar() {
 stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+
+sr.reveal(".showcase-info", { delay:600 });
+sr.reveal(".showcase-image", { origin:"top", delay:700 });
